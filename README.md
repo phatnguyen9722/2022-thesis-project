aa3d9c39b05adf612ce06e89067d28dfb30821ea
Phat Nguyen <phatnguyen.9722@gmail.com>
2023-07-25 21:51:29 +0700

    ENG-00000: Eye detection

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# This is my project for thesis
+# This is my project for Thesis
+
+### Create virtual environment
+```
+python3 -m venv venv`
+source venv/bin/activate
+
+```
+
+### Install packages from requirements.txt
+```
+pip install -r requirements.txt
+
+```
---
+++ app.py
@@ -0,0 +1,16 @@
+from det_eyes import det_eyes
+
+if __name__ == "__main__":
+    print("-------------------------------------------")
+    print("Welcome to Drossiness Detection Application")
+    print("-------------------------------------------")
+    print("""
+    Choose your options:
+    1. Detect Eyes
+    """)
+    answer = input("Your option > ")
+    if answer == "1":
+        COUNTER = 0
+        det_eyes()
+    else: 
+        print("Update next season...")
